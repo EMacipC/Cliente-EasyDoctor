7d4b8bd01bda607ee225519b874a9f1900cb3ba9
EliasM <elias.macip.1995@gmail.com>
2020-04-15 17:51:49 -0500

    unirse

--- src/main/resources/static/js/scriptUnirConsSecre.js
+++ src/main/resources/static/js/scriptUnirConsSecre.js
@@ -4,20 +4,52 @@ function unirse(){
 	var clave=document.getElementById("calve").value;
 	document.getElementById("consultorio").value="";
 	document.getElementById("clave").value="";
-	optenerCon();
+	optenerCon(cons,clave);
 }
-function optenerCon(){
+function optenerCon(cons,clave){
 var request= new XMLHttpRequest();
 	
 	request.onreadystatechange= function(){
 		if(this.readyState==4 && this.status==200 ){
 			var respuesta=JSON.parse(this.responseText);
-			procesarDatos(respuesta);
+			procesarDatos(respuesta,cons,clave);
 		}
 	}
 	request.open("GET","http://localhost:8080/consultorio",false);
 	request.send();
 }
-function procesarDatos(){
+function procesarDatos(consultorios,cons,clave){
+	for(let consu of consultorios){
+		comparar(consu,cons,clave);
+	}
 	
 }
+function comparar(consu){
+	
+	if(consu.nombre==cons&&consu.claveAcceso==calve){
+		var nuevoR={
+				idConsultorio:consu.id,
+				idSecretaria:idUsuario
+		}
+		var request= new XMLHttpRequest();
+		var form=JSON.stringify(nuevoR);
+		request.onreadystatechange= function(){
+			if(this.readyState==4 && this.status==200 ){
+				var respuesta=JSON.parse(this.responseText);
+				
+			}
+		}
+		request.open("POST","http://localhost:8080/doctor",true);
+		request.setRequestHeader("Content-type", "application/json");
+		request.send(form);
+	}
+	
+	
+}
+
+
+
+
+
+
+
